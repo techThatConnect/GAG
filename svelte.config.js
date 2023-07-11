72c18add769fa663c440ca5a2b9de6d030c3a130
Tech That Connect <110293765+techThatConnect@users.noreply.github.com>
2023-07-11 15:37:40 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,16 +6,7 @@ import adapter from '@sveltejs/adapter-netlify';
 const config = {
     kit: {
         // default options are shown
-        adapter: adapter({
-            // if true, will create a Netlify Edge Function rather
-            // than using standard Node-based functions
-            edge: false,
-
-            // if true, will split your app into multiple functions
-            // instead of creating a single one for the entire app.
-            // if `edge` is true, this option cannot be used
-            split: false
-        })
+        adapter: adapter()
     },
 	preprocess: vitePreprocess()
 };
