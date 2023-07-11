450724d6ac467a5754b7abcc1adf55252a1ee82c
Tech That Connect <110293765+techThatConnect@users.noreply.github.com>
2023-07-11 13:36:37 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '550px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+      
+    colors: {
+      
+        purp : '#93639d',
+        light_pink : '#fde0ef',
+       dark_pink :'#f4b6e7',
+         dark : '#26152f',
+         light :  '#fffffe',
+         black : '#000000',
+         grey: '#cfc8cd'
+
+    },
+    
+  },
+  plugins: [],
+}
+
