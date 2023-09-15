7a73876c14cefa000c73bf0605bc0c47bb10927e
Tech That Connect <110293765+techThatConnect@users.noreply.github.com>
2023-09-15 01:15:33 -0300

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@ import pic from '$lib/images/image4.png'
 <body class="h-screen bg-gradient-to-t from-light to-grey dark:bg-black">
    
 <br>
-   <div class="p-4 xl:flex border-2 m-4 text-center  bg-gradient-to-t from-light_pink to-light dark:from-purp dark:to-dark  border-4 border-black rounded-3xl ">
+   <div class="p-4 xl:flex border-2 m-4 text-center  bg-gradient-to-t from-light_pink to-light dark:from-purp dark:to-dark  border-4 border-black">
    <h1 class="lg:text-9xl text-3xl  text-transparent font-bold bg-gradient-to-l from-dark to-purp dark:from-light_pink to-purp  dark:to-dark_pink bg-clip-text">About us</h1>
 </div>
 
